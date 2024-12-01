d64cdff5d0f765379db541c8d4963d17115db809
Ritwik Pai <ritwikpai777@gmail.com>
2024-12-02 04:33:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  // function for the countries
+  const [countries, setCountries] = useState([]);
+
+  // for error handling
+  const [error, setError] = useState(null);
+
+  // fetching the countries data
+  useEffect(() => {
+    axios
+      .get("https://xcountries-backend.azurewebsites.net/all")
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((err) => {
+        console.error("Error while fetching data: ", err);
+        setError("Failed to load country data.");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Flags of Different Countries of the World</h1>
+      {error && <p>{error}</p>}
+
+      <div className="flag-container">
+        {countries.map((country) => (
+          <div key={country.abbr} className="flag-item">
+            <div className="flags">
+              <img
+                src={country.flag}
+                alt={`Flag of ${country.name}`}
+                className="flag-img"
+              ></img>
+            </div>
+            <p className="country-name">{country.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
