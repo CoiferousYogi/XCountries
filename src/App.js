a3e0dd13faaee9fc5958888f988b9592ef0ceca0
Ritwik Pai <ritwikpai777@gmail.com>
2024-12-02 14:51:21 +0530

    Bug fixes of API Call errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 
 function App() {
   // function for the countries
@@ -11,50 +10,46 @@ function App() {
 
   // fetching the countries data
   useEffect(() => {
-    axios
-      .get("https://xcountries-backend.azurewebsites.net/all")
-      .then((response) => {
-        setCountries(response.data);
-      })
-      .catch((err) => {
-        console.error("Error fetching data: ", err);
-        setError("Failed to load country data.");
-      });
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(
+          "https://xcountries-backend.azurewebsites.net/all"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        setError(error.message);
+        console.error("Error fetching data: ", error.message);
+      }
+    };
+
+    fetchCountries();
   }, []);
-
-  window.addEventListener("unhandledrejection", (event) => {
-    console.error("Error fetching data: ", event.reason);
-    setError("Error while fetching data.");
-  });
-
-  axios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      console.error("Error fetching data: ", error);
-      setError("Error while fetching data.");
-      return Promise.reject(error);
-    }
-  );
 
   return (
     <div className="App">
       <h1>Flags of Different Countries of the World</h1>
-      {error && <p>{error}</p>}
-
-      <div className="flag-container">
-        {countries.map((country) => (
-          <div key={country.name} className="flag-item">
-            <div className="flags">
-              <img
-                src={country.flag}
-                alt={`Flag of ${country.name}`}
-                className="flag-img"
-              ></img>
+      {error ? (
+        <p className="error-message">Error: {error}</p>
+      ) : (
+        <div className="flag-container">
+          {countries.map((country) => (
+            <div key={country.name} className="flag-item">
+              <div className="flags">
+                <img
+                  src={country.flag}
+                  alt={`Flag of ${country.name}`}
+                  className="flag-img"
+                ></img>
+              </div>
+              <p className="country-name">{country.name}</p>
             </div>
-            <p className="country-name">{country.name}</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
